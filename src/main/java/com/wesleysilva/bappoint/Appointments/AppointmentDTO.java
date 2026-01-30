b313dc310fc9336764d4c660036149bc9e27f235
Wesley Junio <wesjrpiri@gmail.com>
2026-01-30 23:06:25 +0000

    feat: create dto and mapper for appointment domain.

--- src/main/java/com/wesleysilva/bappoint/Appointments/AppointmentDTO.java
+++ src/main/java/com/wesleysilva/bappoint/Appointments/AppointmentDTO.java
@@ -11,7 +11,7 @@ import java.util.UUID;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class AppointmentsDTO {
+public class AppointmentDTO {
     private UUID id;
 
     private String costumerName;
--- src/main/java/com/wesleysilva/bappoint/Appointments/AppointmentMapper.java
+++ src/main/java/com/wesleysilva/bappoint/Appointments/AppointmentMapper.java
@@ -1,4 +1,34 @@
 package com.wesleysilva.bappoint.Appointments;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class AppointmentMapper {
+    public AppointmentModel toEntity(AppointmentDTO appointmentsDTO) {
+        AppointmentModel appointmentModel = new AppointmentModel();
+        appointmentModel.setId(appointmentsDTO.getId());
+        appointmentModel.setCostumerName(appointmentsDTO.getCostumerName());
+        appointmentModel.setCostumerEmail(appointmentsDTO.getCostumerEmail());
+        appointmentModel.setCostumerPhone(appointmentsDTO.getCostumerPhone());
+        appointmentModel.setAppointmentDate(appointmentsDTO.getAppointmentDate());
+        appointmentModel.setStartTime(appointmentsDTO.getStartTime());
+        appointmentModel.setEndTime(appointmentsDTO.getEndTime());
+        appointmentModel.setAppointmentStatus(appointmentsDTO.getAppointmentStatus());
+
+        return appointmentModel;
+    }
+
+    public AppointmentDTO toDto(AppointmentModel appointmentModel) {
+        AppointmentDTO appointmentsDTO = new AppointmentDTO();
+        appointmentsDTO.setId(appointmentModel.getId());
+        appointmentsDTO.setCostumerName(appointmentModel.getCostumerName());
+        appointmentsDTO.setCostumerEmail(appointmentModel.getCostumerEmail());
+        appointmentsDTO.setCostumerPhone(appointmentModel.getCostumerPhone());
+        appointmentsDTO.setAppointmentDate(appointmentModel.getAppointmentDate());
+        appointmentsDTO.setStartTime(appointmentModel.getStartTime());
+        appointmentsDTO.setEndTime(appointmentModel.getEndTime());
+        appointmentsDTO.setAppointmentStatus(appointmentModel.getAppointmentStatus());
+
+        return appointmentsDTO;
+    }
 }
