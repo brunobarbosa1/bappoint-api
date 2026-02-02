aa02e943a526e3020f57bbbf720de93c496a7a8e
Wesley Junio <wesjrpiri@gmail.com>
2026-02-02 21:56:00 +0000

    feat: add method to find date.

--- src/main/java/com/wesleysilva/bappoint/OperatingHours/OperatingHoursRepository.java
+++ src/main/java/com/wesleysilva/bappoint/OperatingHours/OperatingHoursRepository.java
@@ -2,7 +2,10 @@ package com.wesleysilva.bappoint.OperatingHours;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 public interface OperatingHoursRepository extends JpaRepository<OperatingHoursModel, UUID> {
+    List<OperatingHoursDTO> findByDate(LocalDate date);
 }
