3d4bf1d62fb2100affce9841642a85fb7c11b9a6
Wesley Junio <wesjrpiri@gmail.com>
2026-02-03 15:39:40 +0000

    feat: create a method to find off day by date.

--- src/main/java/com/wesleysilva/bappoint/OffDay/OffDaysRepository.java
+++ src/main/java/com/wesleysilva/bappoint/OffDay/OffDaysRepository.java
@@ -2,7 +2,10 @@ package com.wesleysilva.bappoint.OffDay;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 public interface OffDaysRepository extends JpaRepository<OffDaysModel, UUID> {
+    List<OffDaysModel> findByDate(LocalDate date);
 }
