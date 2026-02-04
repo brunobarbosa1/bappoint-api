e81a3039156af1fe24216865222efd7c9f67f068
Wesley Junio <wesjrpiri@gmail.com>
2026-02-04 15:42:31 +0000

    feat: create method findByAppointmentDateAndCompanyId and findByCompanyId.

--- src/main/java/com/wesleysilva/bappoint/Appointments/AppointmentRepository.java
+++ src/main/java/com/wesleysilva/bappoint/Appointments/AppointmentRepository.java
@@ -2,7 +2,12 @@ package com.wesleysilva.bappoint.Appointments;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 public interface AppointmentRepository extends JpaRepository<AppointmentModel, UUID> {
+    List<AppointmentModel> findByAppointmentDateAndCompanyId(LocalDate date, UUID companyId);
+
+    List<AppointmentModel> findByCompanyId(UUID companyId);
 }
