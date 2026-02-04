411f1dd79635fc73bd767913b3dd6dba24ebbb8f
Wesley Junio <wesjrpiri@gmail.com>
2026-02-04 15:54:47 +0000

    fix; add params of uuid of settingsId to be more security.

--- src/main/java/com/wesleysilva/bappoint/Services/ServiceRepository.java
+++ src/main/java/com/wesleysilva/bappoint/Services/ServiceRepository.java
@@ -3,8 +3,11 @@ package com.wesleysilva.bappoint.Services;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ServiceRepository extends JpaRepository<ServiceModel, UUID> {
     List<ServiceModel> findBySettingsId(UUID settingsId);
+
+    Optional<ServiceModel> findByIdAndSettingsId(UUID id, UUID settingsId);
 }
