6851d180097a24ed59f21a112c00bede4563b221
Wesley Junio <wesjrpiri@gmail.com>
2026-02-13 17:57:22 +0000

    feat: add new exceptions into the ExceptionHandler.

--- src/main/java/com/wesleysilva/bappoint/infra/RestExceptionHandler.java
+++ src/main/java/com/wesleysilva/bappoint/infra/RestExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.wesleysilva.bappoint.infra;
 
+import com.wesleysilva.bappoint.exceptions.CompanyDeleteException;
+import com.wesleysilva.bappoint.exceptions.CompanyNotFoundException;
 import com.wesleysilva.bappoint.exceptions.EmailAlreadyExistsException;
 import com.wesleysilva.bappoint.exceptions.RestErrorMessage;
 import org.springframework.http.HttpStatus;
@@ -13,9 +15,19 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(EmailAlreadyExistsException.class)
     private ResponseEntity<RestErrorMessage> emailAlreadyExistsResponse(EmailAlreadyExistsException exception) {
-        RestErrorMessage threatResponse = new RestErrorMessage(HttpStatus.CONFLICT, "Email already exists");
+        RestErrorMessage threatResponse = new RestErrorMessage(HttpStatus.CONFLICT, "Email already exists.");
         return ResponseEntity.status(HttpStatus.CONFLICT).body(threatResponse);
     }
 
+    @ExceptionHandler(CompanyNotFoundException.class)
+    private ResponseEntity<RestErrorMessage> companyNotFoundResponse(CompanyNotFoundException exception) {
+        RestErrorMessage threatResponse = new RestErrorMessage(HttpStatus.NOT_FOUND, "Company not found.");
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(threatResponse);
+    }
 
+    @ExceptionHandler(CompanyDeleteException.class)
+    private ResponseEntity<RestErrorMessage> companyDeleteResponse(CompanyDeleteException exception) {
+        RestErrorMessage threatResponse = new RestErrorMessage(HttpStatus.CONFLICT, "Failed to delete company.");
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(threatResponse);
+    }
 }
