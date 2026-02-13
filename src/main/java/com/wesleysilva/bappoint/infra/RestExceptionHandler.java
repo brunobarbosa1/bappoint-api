d847ca8e9d74e52d17472ed5bd82c103a361bcbf
Wesley Junio <wesjrpiri@gmail.com>
2026-02-13 12:47:51 +0000

    feat: create handler for personal exceptions.

--- src/main/java/com/wesleysilva/bappoint/infra/RestExceptionHandler.java
+++ src/main/java/com/wesleysilva/bappoint/infra/RestExceptionHandler.java
@@ -1,4 +1,21 @@
 package com.wesleysilva.bappoint.infra;
 
-public class RestExceptionHandler {
+import com.wesleysilva.bappoint.exceptions.EmailAlreadyExistsException;
+import com.wesleysilva.bappoint.exceptions.RestErrorMessage;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(EmailAlreadyExistsException.class)
+    private ResponseEntity<RestErrorMessage> emailAlreadyExistsResponse(EmailAlreadyExistsException exception) {
+        RestErrorMessage threatResponse = new RestErrorMessage(HttpStatus.CONFLICT, "Email already exists");
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(threatResponse);
+    }
+
+
 }
