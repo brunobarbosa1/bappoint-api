24c3976bda4af5ec81819cd1d131b87fa414bbc5
Wesley Junio <wesjrpiri@gmail.com>
2026-02-16 12:12:03 +0000

    feat: add annotation for repository, @Repository.

--- src/main/java/com/wesleysilva/bappoint/Appointments/AppointmentRepository.java
+++ src/main/java/com/wesleysilva/bappoint/Appointments/AppointmentRepository.java
@@ -1,11 +1,13 @@
 package com.wesleysilva.bappoint.Appointments;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
+@Repository
 public interface AppointmentRepository extends JpaRepository<AppointmentModel, UUID> {
     List<AppointmentModel> findByAppointmentDateAndCompanyId(LocalDate date, UUID companyId);
 
