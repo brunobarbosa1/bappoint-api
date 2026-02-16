b26001d42c3111aa28553713b1c01c0dd13e0183
Wesley Junio <wesjrpiri@gmail.com>
2026-02-16 11:52:30 +0000

    feat: crete new dto for create appointments.

--- src/main/java/com/wesleysilva/bappoint/Appointments/dto/CreateAppointmentDTO.java
+++ src/main/java/com/wesleysilva/bappoint/Appointments/dto/CreateAppointmentDTO.java
@@ -1,4 +1,25 @@
 package com.wesleysilva.bappoint.Appointments.dto;
 
+import com.wesleysilva.bappoint.enums.AppointmentStatus;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Data
 public class CreateAppointmentDTO {
+    private String costumerName;
+    private String costumerEmail;
+    private String costumerPhone;
+    private LocalDate appointmentDate;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private BigDecimal totalAmount;
+    private AppointmentStatus appointmentStatus;
+
+    private List<UUID> serviceIds;
+    private UUID companyId;
 }
