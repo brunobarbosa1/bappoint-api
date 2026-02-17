26b3683fbd1c527030d207c8c8e35dc327a7ffe8
Wesley Junio <wesjrpiri@gmail.com>
2026-02-17 13:15:57 +0000

    feat: create personal exception for conflit with appointments.

--- src/main/java/com/wesleysilva/bappoint/exceptions/AppointmentQueryException.java
+++ src/main/java/com/wesleysilva/bappoint/exceptions/AppointmentQueryException.java
@@ -1,4 +1,7 @@
 package com.wesleysilva.bappoint.exceptions;
 
-public class AppointmentQueryException {
+public class AppointmentQueryException extends RuntimeException {
+    public AppointmentQueryException() {
+        super("Failed to list appointments.");
+    }
 }
