ac3573be3301f71eac6f52bfd33f10f51ba8aa17
Wesley Junio <wesjrpiri@gmail.com>
2026-02-17 18:33:40 +0000

    refactor: add new annotation @Transactional and update new dtos.

--- src/main/java/com/wesleysilva/bappoint/OffDay/OffDaysService.java
+++ src/main/java/com/wesleysilva/bappoint/OffDay/OffDaysService.java
@@ -1,10 +1,17 @@
 package com.wesleysilva.bappoint.OffDay;
 
+import com.wesleysilva.bappoint.Company.CompanyModel;
+import com.wesleysilva.bappoint.Company.CompanyRepository;
+import com.wesleysilva.bappoint.OffDay.dto.CreateOffDayDTO;
+import com.wesleysilva.bappoint.OffDay.dto.OffDaysResponseDTO;
 import com.wesleysilva.bappoint.OperatingHours.OperatingHoursDTO;
 import com.wesleysilva.bappoint.OperatingHours.OperatingHoursModel;
 import com.wesleysilva.bappoint.Settings.SettingsModel;
 import com.wesleysilva.bappoint.Settings.SettingsRepository;
+import com.wesleysilva.bappoint.exceptions.CompanyNotFoundException;
+import com.wesleysilva.bappoint.exceptions.SettingsNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,32 +24,39 @@ public class OffDaysService {
    private final OffDaysRepository offDaysRepository;
    private final OffDaysMapper offDaysMapper;
    private final SettingsRepository settingsRepository;
+   private final CompanyRepository companyRepository;
 
-    public OffDaysService(OffDaysRepository offDaysRepository, OffDaysMapper offDaysMapper, SettingsRepository settingsRepository) {
+    public OffDaysService(OffDaysRepository offDaysRepository, OffDaysMapper offDaysMapper, SettingsRepository settingsRepository, CompanyRepository companyRepository) {
         this.offDaysRepository = offDaysRepository;
         this.offDaysMapper = offDaysMapper;
         this.settingsRepository = settingsRepository;
+        this.companyRepository = companyRepository;
     }
 
-    public OffDaysDTO createOffDays(UUID settingsId, OffDaysDTO offDaysDTO) {
+    @Transactional
+    public CreateOffDayDTO createOffDays(UUID companyId, OffDaysDTO offDaysDTO) {
         OffDaysModel offDaysModel = offDaysMapper.toEntity(offDaysDTO);
 
-        SettingsModel settings = settingsRepository
-                .findById(settingsId)
-                .orElseThrow(() -> new RuntimeException("Settings not found"));
+        CompanyModel company = companyRepository.findById(companyId).orElseThrow(CompanyNotFoundException::new);
+
+        SettingsModel settings = company.getSettings();
+
+        if(settings == null) {
+            throw new SettingsNotFoundException();
+        }
 
         offDaysModel.setSettings(settings);
 
         offDaysModel = offDaysRepository.save(offDaysModel);
 
-        return offDaysMapper.toDto(offDaysModel);
+        return offDaysMapper.toCreate(offDaysModel);
     }
 
-    public List<OffDaysDTO> getAllOffDays(){
+    @Transactional(readOnly = true)
+    public List<OffDaysResponseDTO> getAllOffDays(){
         List<OffDaysModel> offDaysModels = offDaysRepository.findAll();
-
         return offDaysModels.stream()
-                .map(offDaysMapper::toDto)
+                .map(offDaysMapper::toResponse)
                 .collect(Collectors.toList());
     }
 
