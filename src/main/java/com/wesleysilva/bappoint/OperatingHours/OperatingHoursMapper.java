da58b0f6d981435474c176c348d127fc8ac69368
Wesley Junio <wesjrpiri@gmail.com>
2026-02-19 14:50:42 +0000

    feat: create new method for new dtos.

--- src/main/java/com/wesleysilva/bappoint/OperatingHours/OperatingHoursMapper.java
+++ src/main/java/com/wesleysilva/bappoint/OperatingHours/OperatingHoursMapper.java
@@ -1,6 +1,9 @@
 package com.wesleysilva.bappoint.OperatingHours;
 
 import com.wesleysilva.bappoint.OperatingHours.dto.CreateOperatingHoursDTO;
+import com.wesleysilva.bappoint.OperatingHours.dto.OperatingHoursAllDetailsDTO;
+import com.wesleysilva.bappoint.OperatingHours.dto.OperatingHoursResponseDTO;
+import com.wesleysilva.bappoint.OperatingHours.dto.UpdateOperatingHoursDTO;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -18,24 +21,50 @@ public class OperatingHoursMapper {
         return operatingHoursModel;
     }
 
-    public OperatingHoursDTO toDto(OperatingHoursModel operatingHoursModel) {
-        OperatingHoursDTO operatingHoursDTO = new OperatingHoursDTO();
-
-        operatingHoursDTO.setId(operatingHoursModel.getId());
-        operatingHoursDTO.setStart_time(operatingHoursModel.getStartTime());
-        operatingHoursDTO.setEnd_time(operatingHoursModel.getEndTime());
-        operatingHoursDTO.setLunch_start_time(operatingHoursModel.getLunchStartTime());
-        operatingHoursDTO.setLunch_end_time(operatingHoursModel.getLunchEndTime());
-        operatingHoursDTO.setWeekday(operatingHoursModel.getWeekday());
-        operatingHoursDTO.setIs_active(operatingHoursModel.getIsActive());
-
-        return operatingHoursDTO;
-    }
-
     public CreateOperatingHoursDTO toCreate(OperatingHoursModel operatingHoursModel) {
         CreateOperatingHoursDTO operatingHoursDTO = new CreateOperatingHoursDTO();
 
         operatingHoursDTO.setId(operatingHoursModel.getId());
+        operatingHoursDTO.setStartTime(operatingHoursModel.getStartTime());
+        operatingHoursDTO.setEndTime(operatingHoursModel.getEndTime());
+        operatingHoursDTO.setLunchStartTime(operatingHoursModel.getLunchStartTime());
+        operatingHoursDTO.setLunchEndTime(operatingHoursModel.getLunchEndTime());
+        operatingHoursDTO.setWeekday(operatingHoursModel.getWeekday());
+        operatingHoursDTO.setIsActive(operatingHoursModel.getIsActive());
+
+        return operatingHoursDTO;
+    }
+
+    public OperatingHoursResponseDTO toResponse(OperatingHoursModel operatingHoursModel) {
+        OperatingHoursResponseDTO operatingHoursResponse = new OperatingHoursResponseDTO();
+
+        operatingHoursResponse.setStartTime(operatingHoursModel.getStartTime());
+        operatingHoursResponse.setEndTime(operatingHoursModel.getEndTime());
+        operatingHoursResponse.setLunchStartTime(operatingHoursModel.getLunchStartTime());
+        operatingHoursResponse.setLunchEndTime(operatingHoursModel.getLunchEndTime());
+        operatingHoursResponse.setWeekday(operatingHoursModel.getWeekday());
+        operatingHoursResponse.setIsActive(operatingHoursModel.getIsActive());
+
+        return operatingHoursResponse;
+    }
+
+    public OperatingHoursAllDetailsDTO toResponseAllDetails(OperatingHoursModel operatingHoursModel) {
+        OperatingHoursAllDetailsDTO operatingHoursDTO = new OperatingHoursAllDetailsDTO();
+
+        operatingHoursDTO.setId(operatingHoursModel.getId());
+        operatingHoursDTO.setStartTime(operatingHoursModel.getStartTime());
+        operatingHoursDTO.setEndTime(operatingHoursModel.getEndTime());
+        operatingHoursDTO.setLunchStartTime(operatingHoursModel.getLunchStartTime());
+        operatingHoursDTO.setLunchEndTime(operatingHoursModel.getLunchEndTime());
+        operatingHoursDTO.setWeekday(operatingHoursModel.getWeekday());
+        operatingHoursDTO.setIsActive(operatingHoursModel.getIsActive());
+
+        return operatingHoursDTO;
+    }
+
+    public UpdateOperatingHoursDTO toUpdate(OperatingHoursModel operatingHoursModel) {
+        UpdateOperatingHoursDTO operatingHoursDTO = new UpdateOperatingHoursDTO();
+
         operatingHoursDTO.setStartTime(operatingHoursModel.getStartTime());
         operatingHoursDTO.setEndTime(operatingHoursModel.getEndTime());
         operatingHoursDTO.setLunchStartTime(operatingHoursModel.getLunchStartTime());
