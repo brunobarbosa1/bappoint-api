de02b49034196d506e580cc202c55f5d269d4113
Wesley Junio <wesjrpiri@gmail.com>
2026-02-20 12:24:49 +0000

    feat: create new dto to create settings.

--- src/main/java/com/wesleysilva/bappoint/Settings/dto/CreateSettingsDTO.java
+++ src/main/java/com/wesleysilva/bappoint/Settings/dto/CreateSettingsDTO.java
@@ -1,4 +1,36 @@
 package com.wesleysilva.bappoint.Settings.dto;
 
+import com.wesleysilva.bappoint.OffDay.dto.CreateOffDayDTO;
+import com.wesleysilva.bappoint.OperatingHours.dto.CreateOperatingHoursDTO;
+import com.wesleysilva.bappoint.Services.dto.CreateServiceDTO;
+import com.wesleysilva.bappoint.enums.AppointmentInterval;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.util.List;
+import java.util.UUID;
+
+@Data
 public class CreateSettingsDTO {
+
+    @NotNull(message = "Appointment interval is required")
+    private AppointmentInterval appointmentInterval;
+
+    @NotNull(message = "Max cancellation interval is required")
+    @Min(value = 0, message = "Cancellation interval cannot be negative")
+    private Integer maxCancellationInterval;
+
+    @NotEmpty(message = "Services cannot be empty")
+    @Valid
+    private List<CreateServiceDTO> services;
+
+    @NotEmpty(message = "Operating hours cannot be empty")
+    @Valid
+    private List<CreateOperatingHoursDTO> operatingHours;
+
+    @Valid
+    private List<CreateOffDayDTO> offDays;
 }
